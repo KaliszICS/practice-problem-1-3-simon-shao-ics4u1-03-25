d4b740fc99e021d9bf89d1981d59a27f44b3da53
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-03-05 18:41:51 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,64 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
-
+public static String readFile(String filename){
+	String file = "";
+	FileReader s = null;
+	int c;
 	
+	try{
+		s = new FileReader(filename);
+		while ((c=s.read())!=-1){
+			file+=(char)c;
+	}
+	}
+	catch(IOException e){
+		System.out.println(e);
+	}
+	finally{
+		try{
+			if (s!=null){
+			s.close();
+			}
+	}
+		catch(IOException e){
+		}
+}	
 
+file = file.strip();
+return file;
+	}
+	
+public static String backwardsReadFile(String filename){
+	String file = "";
+	int c;
+	FileReader inputStream = null;
+	
+	try{
+		inputStream = new FileReader(filename);
+		while ((c = inputStream.read())!=-1 ){
+			file = (char)c + file;
+	}
+	}
+	catch(IOException e){
+		System.out.println(e);
+	}
+	finally{
+try{
+	if (inputStream!=null){
+		inputStream.close();
+	}
+}
+catch(IOException e){
+	System.out.println(e);
+}
+	}
+	file = file.strip();
+	return file;
+}
 }
